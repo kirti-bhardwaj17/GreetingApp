39ef718276ce1c16447817c996836afbd11e9faa
kirti-bhardwaj17 <bhardwajkirti17@gmail.com>
2025-03-04 17:18:21 +0530

    Integrating Swagger

--- src/main/java/com/example/greeting/controller/AuthUserController.java
+++ src/main/java/com/example/greeting/controller/AuthUserController.java
@@ -3,6 +3,7 @@ package com.example.greeting.controller;
 import com.example.greeting.dto.AuthUserDTO;
 import com.example.greeting.dto.LoginDTO;
 import com.example.greeting.Services.AuthUserService;
+import io.swagger.v3.oas.annotations.Operation;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -17,12 +18,14 @@ public class AuthUserController {
         this.authUserService = authUserService;
     }
 
+    @Operation(summary = "Register a new user", description = "Creates a new user with email and password.")
     @PostMapping("/register")
     public ResponseEntity<String> registerUser(@Valid @RequestBody AuthUserDTO userDTO) {
         String response = authUserService.registerUser(userDTO);
         return ResponseEntity.status(201).body(response);
     }
 
+    @Operation(summary = "User login", description = "Logs in a user and returns a JWT token.")
     @PostMapping("/login")
     public ResponseEntity<?> loginUser(@Valid @RequestBody LoginDTO loginDTO) {
         String token = authUserService.loginUser(loginDTO);
